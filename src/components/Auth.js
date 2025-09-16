91b20b8647d3451304f1035df697d731486bf8cb
TusharMinche <tusharminche@gmail.com>
2025-09-16 11:03:02 +0530

    Added class name to p tag

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,7 +4,7 @@ const Auth = () => {
     const {isAuthorized, setIsAuthorized} = useContext(AuthContext);
   return (
     <div>
-        <p>{isAuthorized ? "You are now authonticated, you can proceed" : "You are not authonticated"}</p>
+        <p className='authText'>{isAuthorized ? "You are now authonticated, you can proceed" : "you are not authenticated"}</p>
         <input id='cbox' type='checkbox' onChange={() => setIsAuthorized(!isAuthorized)}/>
         <label htmlFor='cbox'>I'm not a robot</label>
     </div>
